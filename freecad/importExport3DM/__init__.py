dadcf698c6caecb8c390c79bd3982008ac2cacb5
Keith Sloan <keithsloan52@icloud.com>
2025-12-02 20:59:14 +0000

    Remove all .DS_Store files from repo

--- freecad/importExport3DM/__init__.py
+++ freecad/importExport3DM/__init__.py
@@ -1,3 +1,5 @@
 import FreeCAD
+print(f"Set Importer and exporter for 3dm")
 FreeCAD.addImportType("3DM importer (*.3dm)","freecad.importExport3DM.import3DM")
 FreeCAD.addExportType("3DM (*.3dm)","freecad.importExport3DM.export3DM")
+FreeCAD.addExportType("3DM (*.3DM)","freecad.importExport3DM.export3DM")
